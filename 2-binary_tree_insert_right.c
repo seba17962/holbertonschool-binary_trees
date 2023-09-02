8f8d251930002be65664c4c9defb039eda0c64d2
seba17962 <isebsebastiansilva@gmail.com>
2023-09-02 17:20:24 +0000

    add a function thats add a new chile node

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,1 +1,35 @@
 #include "binary_trees.h"
+
+/**
+ *  binary_tree_insert_right - inserts a node as the right-child of another node
+ *  @parent: is a pointer to the node to insert the right-child in
+ *  @value: is the value to store in the new node
+ *  Return: a pointer to the created node,
+ *	or NULL on failure or if parent is NULL
+*/
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new_Node = NULL;
+
+	if (parent == NULL)
+		return (NULL);
+
+	new_Node = malloc(sizeof(binary_tree_t));
+	if (new_Node == NULL)
+		return (NULL);
+
+	new_Node->parent = parent;
+	new_Node->n = value;
+	new_Node->right = NULL;
+	new_Node->right = NULL;
+
+	if (parent->right != NULL)
+	{
+		new_Node->right = parent->right;
+		parent->right->parent = new_Node;
+	}
+
+	parent->right = new_Node;
+
+	return (new_Node);
+}
