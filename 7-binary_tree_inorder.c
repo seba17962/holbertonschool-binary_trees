8834e92842a7b11ebab3277fa7470a256a514e67
seba17962 <isebsebastiansilva@gmail.com>
2023-09-03 14:48:04 +0000

    fix betty style

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -8,15 +8,15 @@
 */
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 {
-    if (tree == NULL)
-        return;
-    if (func == NULL)
-        return;
+	if (tree == NULL)
+		return;
+	if (func == NULL)
+		return;
 
-    binary_tree_inorder(tree->left, func);
+	binary_tree_inorder(tree->left, func);
 
-    func(tree->n);
+	func(tree->n);
 
-    binary_tree_inorder(tree->right, func);
+	binary_tree_inorder(tree->right, func);
 
 }
