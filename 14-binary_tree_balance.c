9a48ef1e8c05b6b8574d9bdef576bbf426a60fbd
SantiagoFleitasIbarra <6769@holbertonstudents.com>
2023-09-04 14:23:14 -0700

    fixed bug

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -6,13 +6,18 @@
 */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	int left_height, right_height;
+	int left_height;
+	int right_height;
 
 	if (tree == NULL)
 		return (0);
 
-	left_height = binary_tree_height(tree->left);
-	right_height = binary_tree_height(tree->right);
+	left_height = binary_tree_balance(tree->left);
+	right_height = binary_tree_balance(tree->right);
 
-	return (left_height - right_height);
+	if (left_height == -1 || right_height == -1 || abs(left_height -
+				right_height) > 1)
+		return (-1);
+
+	return (1 + (left_height > right_height ? left_height : right_height));
 }
