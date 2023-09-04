8cbcbf51bf5f378b4ad5a3b51eca6a1f30ae5abd
SantiagoFleitasIbarra <6769@holbertonstudents.com>
2023-09-04 14:54:21 -0700

    fixed bug

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -19,20 +19,32 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	return (right_height + 1);
 }
 /**
+ * binary_tree_size - Function to find the size of a binary tree
+ * @tree: Pointer to the root node of the tree.
+ * Return: The size of the tree.
+*/
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+	return (1 + binary_tree_size(tree->left) +
+			binary_tree_size(tree->right));
+}
+/**
  * binary_tree_is_perfect - Function to check if a binary tree is perfect.
  * @tree: Pointer to the root node of the tree.
  * Return: 1 if the tree is perfect, 0 otherwise.
 */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	size_t height = binary_tree_height(tree);
-	size_t nodes = 1, i;
+	size_t height, size, max_size;
 
 	if (tree == NULL)
 		return (0);
 
-	for (i = 0; i < height; i++)
-		nodes *= 2;
+	height = binary_tree_height(tree);
+	size = binary_tree_size(tree);
+	max_size = (1 << height) - 1;
 
-	return (nodes - 1 == binary_tree_height(tree));
+	return (size == max_size);
 }
