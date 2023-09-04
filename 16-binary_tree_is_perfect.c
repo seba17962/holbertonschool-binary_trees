1b89c3a92e7b03310126427973f4888ef7ecb723
SantiagoFleitasIbarra <6769@holbertonstudents.com>
2023-09-04 14:10:50 -0700

    fixed bug

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -22,7 +22,7 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 				return (1);
 		}
 		if (tree->left == NULL || tree->right == NULL)
-			return (0);
+			return (1);
 		level++;
 		tree = tree->left;
 	}
